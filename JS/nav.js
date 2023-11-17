09b4ce1ca95869a243dded1ae8b17b7dedecb9fd
Rusty-Boi <69038549+Rusty-Boi@users.noreply.github.com>
2023-11-17 14:24:08 +0700

    First Push

---
+++ JS/nav.js
@@ -0,0 +1,12 @@
+
+function gotoHome(){
+    location.replace("index.html")
+}
+
+function gotoAbout(){
+    location.replace("about.html")
+}
+
+function gotoReg(){
+    location.replace("register.html")
+}
---
+++ JS/register.js
@@ -0,0 +1,15 @@
+function TwitterLogin(){
+    window.open("https://twitter.com/?login")
+}
+
+function fbLogin(){
+    window.open("https://www.facebook.com/login.php")
+}
+
+function googleLogin(){
+    window.open("https://accounts.google.com/Login")
+}
+
+function gotoYoutube(){
+    window.open("https://youtube.com")
+}
